9ad3222bac68ce2289534b20e5797a0a3ba5841d
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-04-14 17:40:09 +0900

    feat :: Color add

--- app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/theme/Color.kt
+++ app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.jinyong68.letseatingtime_android_v2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// 메인 컬러
+val Main = Color(0xFFFF3939)
+val Main2 = Color(0xFFFE6B4B)
+val Main3 = Color(0xFFFE8856)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//서브 컬러
+val Grey = Color(0xFFDBDBDB)
+val Placeholder = Color(0x80000000)
+val Black = Color(0xFF303030)
+val White = Color(0xFFFFFFFF)
+val Bg = Color(0xFFF8F8F8)
