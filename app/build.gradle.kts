9fb4a9d1b72ceef4c6d69621d4bfc92e6d4b9f26
bbqcream <imground11@gmail.com>
2025-04-15 09:54:27 +0900

    fix :: version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jinyong68.letseatingtime_android_v2"
-        minSdk = 35
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
