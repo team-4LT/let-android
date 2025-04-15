b2f91c2a2d4eedff384b32d7e91c2bdce898c6d3
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-04-15 09:51:31 +0900

    feat :: LoginTextField

---
+++ app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/compose/TextField.kt
@@ -0,0 +1,55 @@
+package com.jinyong68.letseatingtime_android_v2.ui.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jinyong68.letseatingtime_android_v2.ui.theme.Black
+import com.jinyong68.letseatingtime_android_v2.ui.theme.Grey
+import com.jinyong68.letseatingtime_android_v2.ui.theme.White
+
+@Composable
+fun LoginTextField(
+    modifier: Modifier = Modifier,
+    text: MutableState<String>,
+    placeholder: @Composable (() -> Unit)?,
+    keyboardType: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+
+    ){
+    OutlinedTextField(
+        modifier = modifier
+            .width(344.dp)
+            .height(70.dp)
+            .background(
+                White
+            )
+        ,
+        value = text.value,
+        onValueChange = { inputText : String ->
+            text.value = inputText},
+        placeholder = placeholder,
+        shape = RoundedCornerShape(12.dp),
+        textStyle = TextStyle(
+            fontSize = 16.sp,
+            textAlign = TextAlign.Start,
+            lineHeight = 70.sp // ✅ 텍스트 입력 위치를 placeholder에 맞춤
+        ),
+        singleLine = true,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = Black,
+            unfocusedBorderColor = Grey,
+        ),
+        keyboardOptions = keyboardType
+    )
+}
