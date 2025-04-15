4ba7706301c7869546aefa7924092102be212b91
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-04-15 09:51:56 +0900

    feat :: Login

--- app/src/main/java/com/jinyong68/letseatingtime_android_v2/MainActivity.kt
+++ app/src/main/java/com/jinyong68/letseatingtime_android_v2/MainActivity.kt
@@ -1,47 +1,33 @@
 package com.jinyong68.letseatingtime_android_v2
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jinyong68.letseatingtime_android_v2.ui.screen.Login
 import com.jinyong68.letseatingtime_android_v2.ui.theme.LetsEatingTime_Android_V2Theme
+import com.jinyong68.letseatingtime_android_v2.ui.theme.Main
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LetsEatingTime_Android_V2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Box(modifier = Modifier){
+                Login(modifier = Modifier)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LetsEatingTime_Android_V2Theme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/screen/Login.kt
+++ app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/screen/Login.kt
@@ -2,47 +2,39 @@ package com.jinyong68.letseatingtime_android_v2.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.Placeholder
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.jinyong68.letseatingtime_android_v2.ui.compose.LoginTextField
-import com.jinyong68.letseatingtime_android_v2.ui.theme.Black
-import com.jinyong68.letseatingtime_android_v2.ui.theme.Grey
 import com.jinyong68.letseatingtime_android_v2.ui.theme.Main
 import com.jinyong68.letseatingtime_android_v2.ui.theme.White
 
 
 @Composable
 fun Login(
-    modifier : Modifier = Modifier
+    modifier : Modifier = Modifier,
 ) {
-    var text by remember { mutableStateOf("") }
+    val idText: MutableState<String> = rememberSaveable {
+        mutableStateOf(
+            ""
+        )
+    }
 
     Box(
         modifier = modifier.fillMaxSize().background(
@@ -65,8 +57,8 @@ fun Login(
         ){
             LoginTextField(
                 modifier = modifier,
-                text = "",
-                placeholder = ""
+                text = idText,
+                placeholder = {Text("아이디를 입력해주세요")}
             )
         }
 
