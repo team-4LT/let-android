0fce99f43079c138d1f06270a4ed54bf4805ae91
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-04-22 16:16:33 +0900

    feat :: MealScan

--- app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/screen/Home.kt
+++ app/src/main/java/com/jinyong68/letseatingtime_android_v2/ui/screen/Home.kt
@@ -1,13 +1,22 @@
 package com.jinyong68.letseatingtime_android_v2.ui.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.jinyong68.letseatingtime_android_v2.ScreenNavigate
 
 @Composable
 fun Home(
-    modifier : Modifier = Modifier,
-    onMoveScreen : (String) -> Unit = {}
+    modifier: Modifier = Modifier,
+    onMoveScreen: (String) -> Unit
 ) {
-    Text("안녕")
+    Text(
+        modifier = Modifier.fillMaxSize().statusBarsPadding().clickable{
+            onMoveScreen(ScreenNavigate.MEALSCAN.name)
+        },
+        text = "안녕"
+    )
 }
